5133dedc7a7827ab101d96306d9d4bb7c64bb0a2
Cendrinne <nancy13lg@gmail.com>
2025-10-19 21:17:39 -0400

    fixed typo in context in js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateWisdom(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "73f90ofbe4423241037fca0b58a62fdt";
   let context =
-    "You are a expert and full of wisdom. You mission is in HTML format, generate a 4 line wisdom and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Do not include markdown in your response. Sign the poem with 'SheCodes AI' on it own line, inside a <strong> element at the end of the wisdom and NOT at the beginning";
+    "You are an expert and full of wisdom. You mission is in HTML format, generate a 4 line wisdom and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Do not include markdown in your response. Sign the poem with 'SheCodes AI' on it own line, inside a <strong> element at the end of the wisdom and NOT at the beginning";
   let prompt = `User instructions: Generate Wisdom about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
